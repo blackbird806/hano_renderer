9e0cdd8052dce35e5d529c07b2902201d4ec454f
blackbird <blackbirdcry806@gmail.com>
2020-05-31 00:42:43 +0200

    added default dockspace

--- source/src/renderer/hanoEditor.cpp
+++ source/src/renderer/hanoEditor.cpp
@@ -101,8 +101,59 @@ void HanoEditor::initUI()
 	m_renderer->reloadShaders();
 }
 
+static void initDockspace()
+{
+	static bool opt_fullscreen_persistant = true;
+	bool opt_fullscreen = opt_fullscreen_persistant;
+	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+
+	// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+	// because it would be confusing to have two docking targets within each others.
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+	if (opt_fullscreen)
+	{
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->Pos);
+		ImGui::SetNextWindowSize(viewport->Size);
+		ImGui::SetNextWindowViewport(viewport->ID);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+	}
+
+	// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
+	if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+		window_flags |= ImGuiWindowFlags_NoBackground;
+
+	// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+	// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+	// all active windows docked into it will lose their parent and become undocked.
+	// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+	// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	static bool p_open = true;
+	ImGui::Begin("DockSpace", &p_open, window_flags);
+	ImGui::PopStyleVar();
+
+	if (opt_fullscreen)
+		ImGui::PopStyleVar(2);
+
+	// DockSpace
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+	{
+		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+	}
+
+	ImGui::End();
+}
+
 void HanoEditor::drawUI()
 {
+	initDockspace();
+
 	Scene& scene = *m_renderer->getCurrentScene();
 	ImDrawList* drawList = ImGui::GetBackgroundDrawList();
 
@@ -110,18 +161,12 @@ void HanoEditor::drawUI()
 
 	static bool show_demo = true;
 	static bool show_logger = true;
+	static bool show_scene = true;
 
 	if (show_demo)
 		ImGui::ShowDemoWindow(&show_demo);
 	if (show_logger)
 		logger.draw("Logger", &show_logger);
-
-	if (ImGui::Button("testLog"))
-	{
-		hano_info("salut la {}", "compagnie");
-		hano_warn("warn test {}", 45);
-		hano_error("error test {}", 45.2);
-	}
 
 	if (ImGui::BeginMainMenuBar())
 	{
@@ -129,12 +174,13 @@ void HanoEditor::drawUI()
 		{
 			ImGui::MenuItem("demo", nullptr, &show_demo);
 			ImGui::MenuItem("logger", nullptr, &show_logger);
+			ImGui::MenuItem("scene", nullptr, &show_logger);
 			ImGui::EndMenu();
 		}
 		ImGui::EndMainMenuBar();
 	}
 
-	ImGui::Begin("scene");
+	ImGui::Begin("scene", &show_scene);
 
 	ImGuiIO& io = ImGui::GetIO();
 	ImGui::Text("Dear ImGui %s", ImGui::GetVersion());
